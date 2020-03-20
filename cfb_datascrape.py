b0c068f5f11c88e268dbb2334ffa388d6b5c5f80
stevenbischoff <61480100+stevenbischoff@users.noreply.github.com>
2020-03-20 14:20:32 -0500

    Update cfb_datascrape.py

--- cfb_datascrape.py
+++ cfb_datascrape.py
@@ -51,21 +51,13 @@ def games_scrape(year):
 
 
 def talent_scrape(year):
-  base_url = 'https://api.collegefootballdata.com/talent'
   
+  base_url = 'https://api.collegefootballdata.com/talent'  
   parameters = {'year':year}
+  
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
     return pd.DataFrame(response.json(),columns = ['school','talent']
         ).rename(columns = {'school':'team'})
   else:
-    raise Exception('Request failed with status code: '+str(response.status_code))
-
-  if year == first_year:
-    tot_talent_df = year_talent_df
-  else:
-    tot_talent_df = tot_talent_df.merge(year_talent_df,on='team')
-
-  return tot_talent_df
-
-      
+    raise Exception('Request failed with status code: '+str(response.status_code))     
