7e2aded94aa1ebf83bb29c449b6633170f747e03
stevenbischoff <61480100+stevenbischoff@users.noreply.github.com>
2020-03-23 15:04:26 -0500

    Changed parameter names: year -> season

--- cfb_datascrape.py
+++ cfb_datascrape.py
@@ -2,15 +2,15 @@ import requests
 import pandas as pd
 
 
-def adv_data_scrape(year):
+def adv_data_scrape(season):
   
   """
-  Returns advanced game stats pulled using the collegefootballdata API. 
+  Returns advanced game stats for a season pulled using the collegefootballdata API. 
   The function concatenates regular season and postseason stats into a single Pandas DataFrame.  
   """
 
   base_url = 'https://api.collegefootballdata.com/stats/game/advanced'    
-  parameters = {'year':year,'seasonType':'regular'}
+  parameters = {'year':season,'seasonType':'regular'}
   
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
@@ -19,7 +19,7 @@ def adv_data_scrape(year):
   else:
     raise Exception('Request failed with status code: '+str(response.status_code))
  
-  parameters = {'year':year,'seasonType':'postseason'}
+  parameters = {'year':season,'seasonType':'postseason'}
   
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
@@ -31,17 +31,17 @@ def adv_data_scrape(year):
   return pd.concat([reg_df,post_df])
 
 
-def games_scrape(year):
+def games_scrape(season):
   
   """
-  Returns a DataFrame of game information that includes the conferences of the participants,
+  Returns a DataFrame of game information for a season that includes the conferences of the participants,
   whether the game was played on a neutral field, and the final score, all of which are relevant for downstream
   data manipulation and training.
   Also concatenates regular season and postseason information.
   """
 
   base_url = 'https://api.collegefootballdata.com/games'
-  parameters = {'year':year,'seasonType':'regular'}
+  parameters = {'year':season,'seasonType':'regular'}
   
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
@@ -50,7 +50,7 @@ def games_scrape(year):
   else:
     raise Exception('Request failed with status code: '+str(response.status_code))
  
-  parameters = {'year':year,'seasonType':'postseason'}
+  parameters = {'year':season,'seasonType':'postseason'}
   
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
@@ -62,15 +62,15 @@ def games_scrape(year):
   return pd.concat([reg_games_df,post_games_df])
 
 
-def talent_scrape(year):
+def talent_scrape(season):
   
   """
-  Returns a DataFrame with 247 composite talent ratings, which only go back through 2015.
+  Returns a DataFrame with 247 composite talent ratings for a season, which only go back through 2015.
   """
   
   
   base_url = 'https://api.collegefootballdata.com/talent'  
-  parameters = {'year':year}
+  parameters = {'year':season}
   
   response = requests.get(base_url,params=parameters)
   if response.status_code == 200:
